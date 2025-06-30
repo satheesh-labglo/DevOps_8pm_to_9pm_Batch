c406d7c9203eff30358fd28b2b4668b4284ed183
satheesh-labglo <satheeshkumar.labglo@gmail.com>
2025-06-30 12:20:41 +0530

    chaged PORT number: 80 = 5001

--- Docker/Dockerfile_for_pythonFlask_App/app.py
+++ Docker/Dockerfile_for_pythonFlask_App/app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask # type: ignore
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Welcome to Docker World By SRTechOps !!</h1>"
+    return "<h1>Welcome to Sk!</h1>"
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=True,host='0.0.0.0',port=port)
